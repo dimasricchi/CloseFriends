3bb6c02bf8092ab2ec7f625a046d2892ffd00cd3
dimasricchi <36904123+dimasricchi@users.noreply.github.com>
2018-03-03 19:32:57 -0300

    Adicionar arquivos de projeto.

---
+++ CloseFriends/Friend.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseFriends
+{
+    class Friend
+    {
+        public int X;
+        public int Y;
+        public int indexEuclideanDistance;
+
+        public Friend[] closeFriends = new Friend[3];
+
+        public Friend(int count)
+        {
+            indexEuclideanDistance = count;
+        }
+
+        public Friend(int x, int y, int count)
+        {
+            X = x;
+            Y = y;
+            indexEuclideanDistance = count;
+
+            closeFriends[0] = new Friend(count);
+            closeFriends[1] = new Friend(count);
+            closeFriends[2] = new Friend(count);
+        }
+    }
+}
---
+++ CloseFriends/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+
+namespace CloseFriends
+{
+    class Program
+    {
+        private const int COUNT = 9;
+
+        static void Main(string[] args)
+        {
+            Friend [] friends = new Friend[COUNT];
+
+            Random random = new Random();
+
+            for(int i = 0; i < COUNT; i++)
+            {
+                friends[i] = new Friend(random.Next()%100, random.Next()%100, COUNT);
+            }
+
+            try
+            {
+                for (int i = 0; i < COUNT; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        int current = int.MaxValue;
+                        int index_k = COUNT;
+
+                        for (int k = 0; k < COUNT; k++)
+                        {
+                            if (i != k && k != friends[i].closeFriends[0].indexEuclideanDistance
+                                && k != friends[i].closeFriends[1].indexEuclideanDistance
+                                && k != friends[i].closeFriends[2].indexEuclideanDistance)
+                            {
+                                int euclideanDistance = (int)(Math.Sqrt(Math.Pow(friends[i].X - friends[k].X, 2) + Math.Pow(friends[i].Y - friends[k].Y, 2)));
+
+                                if (euclideanDistance < current)
+                                {
+                                    current = euclideanDistance;
+                                    index_k = k;
+                                }
+                            }
+                        }
+                        if (index_k < COUNT)
+                        {
+                            friends[i].closeFriends[j].X = friends[index_k].X;
+                            friends[i].closeFriends[j].Y = friends[index_k].Y;
+                            friends[i].closeFriends[j].indexEuclideanDistance = index_k;
+                        }
+                    }
+                }
+                for (int i = 0; i < COUNT; i++)
+                {
+                    Console.WriteLine(string.Format("[{0},{1}] - [{2},{3}] [{4},{5}] [{6},{7}]",
+                        friends[i].X, friends[i].Y,
+                        friends[i].closeFriends[0].X, friends[i].closeFriends[0].Y,
+                        friends[i].closeFriends[1].X, friends[i].closeFriends[1].Y,
+                        friends[i].closeFriends[2].X, friends[i].closeFriends[2].Y));
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
